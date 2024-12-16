03ca828a16d952c68e4e76d8352d1055cb361362
jakub <115490190+jakubpietraszko@users.noreply.github.com>
2024-12-16 13:13:06 +0100

    save

--- lib/utils.py
+++ lib/utils.py
@@ -11,13 +11,13 @@ import torchvision.transforms
 import tensorflow as tf
 
 
-
 from PIL import Image
 import torchvision.transforms as T
 import torch
 
+
 def get_image_torch(path: Path, x: int, y: int) -> torch.Tensor:
-    '''
+    """
     Function to read image from path and resize it to x, y
 
     Args:
@@ -27,14 +27,15 @@ def get_image_torch(path: Path, x: int, y: int) -> torch.Tensor:
 
     Returns:
         torch.Tensor: Image tensor with shape (3, x, y)
-    '''
+    """
     img = Image.open(path).convert("RGB")
     img = img.resize((x, y))
     img = torchvision.transforms.ToTensor()(img)
     return img
 
+
 def get_image_tf(path: Path, x: int, y: int) -> tf.Tensor:
-    '''
+    """
     Function to read image from path and resize it to x, y
 
     Args:
@@ -44,11 +45,12 @@ def get_image_tf(path: Path, x: int, y: int) -> tf.Tensor:
 
     Returns:
         tf.Tensor: Image tensor with shape (x, y, 3)
-    '''
+    """
     img = tf.io.read_file(str(path))
     img = tf.image.decode_image(img)
     img = tf.image.resize(img, (x, y))
     return img
+
 
 """def overlay_plot(path: Path, cam: np.ndarray, alpha: float) -> None:
     '''
@@ -88,8 +90,10 @@ def get_image_tf(path: Path, x: int, y: int) -> tf.Tensor:
     plt.show()"""
 
 
-def overlay_plot_torch(image: torch.Tensor, cam: np.ndarray, alpha: float, save_path: Path = None) -> None:
-    '''
+def overlay_plot_torch(
+    image: torch.Tensor, cam: np.ndarray, alpha: float, save_path: Path = None
+) -> None:
+    """
     Function to overlay CAM on image
     It shows the image, overlayed image, and CAM.
 
@@ -100,12 +104,14 @@ def overlay_plot_torch(image: torch.Tensor, cam: np.ndarray, alpha: float, save_
 
     Returns:
         None
-    '''
+    """
     img = image.permute(1, 2, 0).numpy()
     img = cv2.normalize(img, None, 0, 255, cv2.NORM_MINMAX).astype(np.uint8)
 
     cam_resized = cv2.resize(cam, (img.shape[1], img.shape[0]))
-    cam_resized = cv2.normalize(cam_resized, None, 0, 255, cv2.NORM_MINMAX).astype(np.uint8)
+    cam_resized = cv2.normalize(cam_resized, None, 0, 255, cv2.NORM_MINMAX).astype(
+        np.uint8
+    )
 
     # Invert the heatmap values
     cam_resized = 255 - cam_resized
@@ -116,16 +122,15 @@ def overlay_plot_torch(image: torch.Tensor, cam: np.ndarray, alpha: float, save_
 
     fig, ax = plt.subplots(1, 3, figsize=(15, 5))
     ax[0].imshow(img)
-    ax[0].axis('off')
+    ax[0].axis("off")
 
     ax[1].imshow(overlayed)
-    ax[1].axis('off')
+    ax[1].axis("off")
 
     cam_resized = 255 - cam_resized
 
-
-    ax[2].imshow(cam_resized, cmap='jet')
-    ax[2].axis('off')
+    ax[2].imshow(cam_resized, cmap="jet")
+    ax[2].axis("off")
 
     if save_path:
         plt.savefig(save_path)
@@ -133,8 +138,41 @@ def overlay_plot_torch(image: torch.Tensor, cam: np.ndarray, alpha: float, save_
     plt.show()
 
 
+def get_overlay(image: torch.Tensor, cam: np.ndarray, alpha: float) -> torch.Tensor:
+    """
+    Function to return ready overlay image
+
+    Args:
+        image (torch.Tensor): Image tensor with shape (3, x, y)
+        cam (np.ndarray): CAM with shape (x_, y_) x_ and y_ smaller than x and y
+        alpha (float): Alpha value for overlay
+
+    Returns:
+        torch.Tensor: Image tensor with shape (3, x, y)
+    """
+
+    img = image.permute(1, 2, 0).numpy()
+    img = cv2.normalize(img, None, 0, 255, cv2.NORM_MINMAX).astype(np.uint8)
+
+    cam_resized = cv2.resize(cam, (img.shape[1], img.shape[0]))
+    cam_resized = cv2.normalize(cam_resized, None, 0, 255, cv2.NORM_MINMAX).astype(
+        np.uint8
+    )
+
+    # Invert the heatmap values
+    cam_resized = 255 - cam_resized
+
+    heatmap = cv2.applyColorMap(cam_resized, cv2.COLORMAP_JET)
+
+    overlayed = cv2.addWeighted(img, alpha, heatmap, 1 - alpha, 0)
+
+    cam_resized = 255 - cam_resized
+
+    return overlayed
+
+
 def plot_torch(image: torch.Tensor, save_path: Path = None) -> None:
-    '''
+    """
     Function to plot image
 
     Args:
@@ -142,19 +180,22 @@ def plot_torch(image: torch.Tensor, save_path: Path = None) -> None:
 
     Returns:
         None
-    '''
+    """
     img = image.permute(1, 2, 0).numpy()
 
     plt.imshow(img)
-    plt.axis('off')
+    plt.axis("off")
 
     if save_path:
         plt.savefig(save_path)
 
     plt.show()
 
-def delete_the_least_important_pixels(image: torch.Tensor, cam: np.ndarray, percentage: float) -> torch.Tensor:
-    '''
+
+def delete_the_least_important_pixels(
+    image: torch.Tensor, cam: np.ndarray, percentage: float
+) -> torch.Tensor:
+    """
     Function to delete the least important pixels from the image
     It uses CAM to determine which pixels are the least important
 
@@ -165,10 +206,12 @@ def delete_the_least_important_pixels(image: torch.Tensor, cam: np.ndarray, perc
 
     Returns:
         torch.Tensor: Image tensor with shape (3, x, y)
-    '''
+    """
 
     cam_resized = cv2.resize(cam, (image.shape[2], image.shape[1]))
-    cam_resized = cv2.normalize(cam_resized, None, 0, 255, cv2.NORM_MINMAX).astype(np.uint8)
+    cam_resized = cv2.normalize(cam_resized, None, 0, 255, cv2.NORM_MINMAX).astype(
+        np.uint8
+    )
     mask = cam_resized <= int(percentage * 255)
 
     img_np = image.permute(1, 2, 0).cpu().numpy()
@@ -176,8 +219,9 @@ def delete_the_least_important_pixels(image: torch.Tensor, cam: np.ndarray, perc
 
     return torch.from_numpy(img_np).permute(2, 0, 1).float()
 
+
 def average_drop(Y: np.ndarray, O: np.ndarray) -> float:
-    '''
+    """
     Function to calculate average drop in output values
 
     Args:
@@ -186,11 +230,12 @@ def average_drop(Y: np.ndarray, O: np.ndarray) -> float:
 
     Returns:
         float: Average drop in output values
-    '''
+    """
     return np.maximum(0, (Y - O)).sum() / Y.shape[0]
 
+
 def rate_of_increase_in_score(Y: np.ndarray, O: np.ndarray) -> float:
-    '''
+    """
     Function to calculate rate of increase in score
 
     Args:
@@ -199,23 +244,24 @@ def rate_of_increase_in_score(Y: np.ndarray, O: np.ndarray) -> float:
 
     Returns:
         float: Rate of increase in score
-    '''
+    """
     return (Y < O).sum() / Y.shape[0]
+
 
 id_name = dict()
 dups_id_name = dict()
 
-for i, name in enumerate(open('imagenet_classes.txt', 'r')):
+for i, name in enumerate(open("imagenet_classes.txt", "r")):
     name = name[:-1]
     name = name.lower()
-    name = name.replace(' ', '_')
+    name = name.replace(" ", "_")
     id_name[i] = name
 
 len(id_name)
 hash_name = dict()
 dups_hash_name = dict()
 
-for line in open('imagenet.txt', 'r'):
+for line in open("imagenet.txt", "r"):
     line = line[:-1]
     h, i, n = line.split()
     n = n.lower()
